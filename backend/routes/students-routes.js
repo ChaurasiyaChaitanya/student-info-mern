953564a4e24a462cc85fb3954506f4b9f547d0b6
Chaitanya Chaurasiya <1011chaitanya@gmail.com>
2024-01-12 21:20:51 +0530

    Added search and sort functionality

--- backend/routes/students-routes.js
+++ backend/routes/students-routes.js
@@ -6,8 +6,36 @@ const studentRouter = express.Router();
 
 studentRouter.get('/', async (req, res) => {
  
+    let data;
+    let search = req.query.search;
+    let sortBy = req.query.sortBy;
+    let sortType = req.query.sortType;
+ 
+    let ord = 1;
+    if(sortType && sortType.charAt(0) === "d") {
+        ord = -1;
+    }
+ 
     try {
-        const data = await Student.find();
+        if(sortBy && sortBy.includes('Roll')) {
+            data = await Student.find().sort({roll_number: ord});
+            console.log(data);
+        } else if (sortBy && sortBy.includes('Name')) {
+            data = await Student.find().sort({student_name: ord});
+            console.log(data);
+        } else {
+            data = await Student.find();
+        }
+        if(search) {
+            data = await Student.find({$or : [{student_name : {$regex : search , $options : 'i'}}, {roll_number : {$regex : search , $options : 'i'}}]})
+            // data = data.filter((d) => d.roll_number.toLowerCase().includes(search.toLowerCase())
+            // || d.student_name.toLowerCase().includes(search.toLowerCase()));
+ 
+            // starts with '^'+search
+            // ends with search+'$'
+            console.log(data);
+        }
+ 
         res.status(200).json(data);
     } catch (err) {
         res.status(400).json(err)
--- student-info/src/components/Home.jsx
+++ student-info/src/components/Home.jsx
@@ -7,10 +7,13 @@ import axios from "axios";
 function Home() {
 
     const [stuData, setStuData] = useState([]);
+    const [search, setSearch] = useState("");
+    const [sortBy, setSortBy] = useState("");
+    const [sortType, setSortType] = useState("");
 
     const handleGetData = async () => {
         try {
-            const res = await axios.get(`http://localhost:5000/students`);
+            const res = await axios.get(`http://localhost:5000/students?search=${search}&sortBy=${sortBy}&sortType=${sortType}`);
             setStuData(res.data);
         } catch (error) {
             console.log(error.message);
@@ -20,7 +23,7 @@ function Home() {
 
     useEffect(() => {
         handleGetData();
-    }, []);
+    }, [search, sortBy, sortType]);
 
 
 
@@ -28,10 +31,30 @@ function Home() {
         <>
             <div className="container mt-4">
                 <div className="d-flex justify-content-between">
+                    <div className="row g-3">
+                        <div className="col-auto">
+                            <input type="text" className="form-control" name="search" placeholder="Search" onChange={(e) => setSearch(e.target.value)} />
+                        </div>
+                    </div>
+                    <div className="col-sm-3 d-flex justify-content-between">
+                        <div className="form-check p-2">
+                            <input className="form-check-input" type="radio" name="sortBy" value="sortRoll" onChange={(e) => setSortBy(e.target.value)} />
+                            <label htmlFor="sortBy1" className="form-check-label">Roll Number</label>
+                        </div>
+                        <div className="form-check p-2">
+                            <input className="form-check-input" type="radio" name="sortBy" value="sortName" onChange={(e) => setSortBy(e.target.value)} />
+                            <label htmlFor="sortBy2" className="form-check-label">Student Name</label>
+                        </div>
+                    </div>
+                    <select className="col-sm-2 form-select" onChange={(e) => setSortType(e.target.value)}>
+                        <option value="">Select Sort Type</option>
+                        <option value="asc">Ascending</option>
+                        <option value="desc">Descending</option>
+                    </select>
                     <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#studentModal">Add Student Data</button>
                 </div>
-                <Register handleGetData={handleGetData}/>
-                <Table data={stuData}/>
+                <Register handleGetData={handleGetData} />
+                <Table data={stuData} />
             </div>
         </>
     );
